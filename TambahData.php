62a7aed06527047b335897975f3d85686fdb6123
HammamRahman <164612419+HammamRahman@users.noreply.github.com>
2024-08-30 15:47:26 +0700

    Update TambahData.php

--- TambahData.php
+++ TambahData.php
@@ -44,17 +44,17 @@ mysqli_close($conn);
 </head>
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand ps-3" href="dashboard.php">ContractManagement</a>
+        <a class="navbar-brand ps-3" href="data.php">ContractManagement</a>
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
         </form>
     </nav>
     <div id="layoutSidenav">
@@ -62,10 +62,10 @@ mysqli_close($conn);
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
                     <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="dashboard.php">
+                        <div class="sb-sidenav-menu-heading"> </div>
+                        <a class="nav-link" href="data.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Dashboard
+                            Data Pelanggan
                         </a>
                         <a class="nav-link" href="tambahdata.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
@@ -80,7 +80,7 @@ mysqli_close($conn);
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">Tambah Data</h1>
                     <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="data.php">Data Pelanggan</a></li>
                         <li class="breadcrumb-item active">Tambah Data</li>
                     </ol>
 
